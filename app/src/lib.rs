cc4938c60b401fb1a2ead070d41f7355151dafb6
AlanRandon <alan.randon@outlook.com>
2022-12-05 16:25:45 +0000

    Removed counter bg

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -16,10 +16,10 @@ pub fn App<G: Html>(cx: Scope, props: AppProps) -> View<G> {
     let increment = |_| count.set(*count.get() + 1);
     let decrement = |_| count.set(*count.get() - 1);
     view! { cx,
-        div(class="bg-slate-800/50 rounded shadow text-white/70 p-4 flex gap-4 items-center") {
-            button(class="bg-slate-800 rounded shadow p-4 transition-colors hover:bg-slate-700", on:click=decrement) { "-" }
+        div(class="text-white/70 p-4 flex gap-4 items-center") {
+            button(class="bg-slate-800 rounded shadow p-4 transition-colors hover:bg-slate-900", on:click=decrement) { "-" }
             p { "Count: " (count.get()) }
-            button(class="bg-slate-800 rounded shadow p-4 transition-colors hover:bg-slate-700", on:click=increment) { "+" }
+            button(class="bg-slate-800 rounded shadow p-4 transition-colors hover:bg-slate-900", on:click=increment) { "+" }
         }
     }
 }
